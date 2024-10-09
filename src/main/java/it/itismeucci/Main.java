74fa0daa07c231d8fdbe68eb8e9671fcd64d57fe
MattiaCugusi <m.cugusi287@gmail.com>
2024-10-09 09:19:00 +0200

    trasmissione stringa

--- src/main/java/it/itismeucci/Main.java
+++ src/main/java/it/itismeucci/Main.java
@@ -1,7 +1,28 @@
 package it.itismeucci;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        System.out.println("Il client e' partito");
+        Socket mySocket = new Socket("localhost", 5673);
+        System.out.println("il client si e' collegato");
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
+        DataOutputStream out = new DataOutputStream(mySocket.getOutputStream());
+        
+        out.writeBytes("prova collegamento \n");
+
+        String stringaRicevuta = in.readLine();
+
+        System.out.println(stringaRicevuta);
+
+        mySocket.close();
+
     }
-}
+} 
